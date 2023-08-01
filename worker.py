9127664a563684702abdc1e429d96ffe25e0e11a
Y. Jace Liu <yang.jace.liu@linux.com>
2023-07-31 18:54:17 -0600

    Fix Async issue
    Signed-off-by: Y. Jace Liu <yang.jace.liu@linux.com>

--- worker.py
+++ worker.py
@@ -9,11 +9,13 @@ class WorkerThread(threading.Thread):
     def __init__(self, async_task_pool_size: int = 5):
         super().__init__()
         self.async_task_pool_size = async_task_pool_size
+        self.loop: asyncio.AbstractEventLoop = None
 
     def run(self) -> None:
         asyncio.run(self.async_main())
 
     async def async_main(self):
+        self.loop = asyncio.get_running_loop()
         self.queue = asyncio.LifoQueue()
         self.sem = asyncio.Semaphore(self.async_task_pool_size)
         while True:
@@ -28,6 +30,8 @@ class WorkerThread(threading.Thread):
         self.sem.release()
 
     def lookup_word(self, word: str):
-        if Config.DEBUG_MODE:
-            print(f"Start looking up for word: {word}")
-        self.queue.put_nowait(word)
+        async def lookup_word_async(word: str):
+            if Config.DEBUG_MODE:
+                print(f"Start looking up for word: {word}")
+            await self.queue.put(word)
+        asyncio.run_coroutine_threadsafe(lookup_word_async(word), self.loop)
